75c643918bd817c2a7e3a5e33e9b555f91f667a0
Matthew Qualls <mqggd@mst.edu>
2017-02-21 17:53:47 -0600

    Changed docker to pull Stable DB

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker pull siggame/colisee-db
+docker pull siggame/colisee-db:stable
 docker rm --force tournament-db
-docker run --name tournament-db --publish 5432:5432 --detach siggame/colisee-db
+docker run --name tournament-db --publish 5432:5432 --detach siggame/colisee-db:stable
 
 npm run test
 
