54db2145a9bed8536eb407b059095283967f2379
user404d <conduffql@gmail.com>
2017-09-13 04:07:47 -0500

    Add delay helper

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,7 @@
 export function catchError<T>(fn: (...args: any[]) => Promise<T>) {
     return async (...args: any[]) => fn(...args).catch(args[2]);
 }
+
+export async function delay(ms: number) {
+    return new Promise((res, rej) => { setTimeout(res, ms); });
+}
